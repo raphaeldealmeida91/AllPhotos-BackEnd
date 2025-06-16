922f3ad79f25591b026b92d0706c07e2753cde59
Raphalinho91 <lokiju1598@outlook.fr>
2025-06-16 22:33:59 +0200

    New route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ import GetImagesByFolderWithWatermark from "../controllers/GetImagesByFolderWith
 import { GetImagesByIdsWithWatermark } from "../controllers/GetImagesByIdsWithWatermark.js";
 
 import PostImagesInFolder from "../controllers/PostImagesInFolder.js";
+import { stripeWebhook } from "../controllers/StripeWebhookSuccess.js";
 
 export default async function routes(fastify, options) {
   fastify.post("/api/images/:folder", PostImagesInFolder);
@@ -16,4 +17,9 @@ export default async function routes(fastify, options) {
   fastify.get("/api/folders/", GetAllFolders);
   fastify.delete("/api/folder/:folder", DeleteOneFolder);
   fastify.post("/api/payment/checkout", createStripeCheckoutSession);
+  fastify.post(
+    "/api/payment/webhook",
+    { bodyLimit: "1mb", rawBody: true },
+    stripeWebhook
+  );
 }
